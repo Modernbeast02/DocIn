0baa9c80a562f903829e4360baae2151cabe24bc
Ankur Gupta <ankurgupta.bt21cse@pec.edu.in>
2023-11-22 10:37:59 +0530

    #minor change

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -9,8 +9,8 @@ const login = () => {
     router.push(`/temp`);
   } else {
     return (
-      <div className="bg-cover h-screen w-full flex justify-center items-center bg-gradient-to-r from-[#de6262] to-[#ffb88c]">
-        <div className="flex justify-center items-center w-[900px] h-[500px] bg-white">
+      <div className="bg-cover h-screen w-full flex justify-center items-center bg-gradient-to-r from-[#f59292] to-[#ffb88c]">
+        <div className="flex justify-center items-center w-[900px] h-[500px] bg-white rounded-xl">
           <div className="flex flex-col w-full h-screen items-center justify-center">
               <div className="px-20 py-4 flex flex-col justify-centerv items-center space-y-4">
                   <h1 className="text-6xl font-semibold text-black uppercase">
@@ -20,7 +20,7 @@ const login = () => {
                 
                </div>
                 <div className="flex justify-center items-center mt-8">
-                  <button onClick={() => signIn()} className="text-white py-3 px-8 flex justify-center items-center rounded-md border-2 bg-gradient-to-r from-[#ff512f] to-[#dd2476] hover:from-pink-500 hover:to-yellow-500 font-bold">
+                  <button onClick={() => signIn()} className="text-white py-3 px-8 flex justify-center items-center rounded-md border-2 bg-gradient-to-r from-[#ff512f] to-[#dd2476] hover:from-pink-500 hover:to-yellow-500 font-bold hover:scale-[1.05] duration-300">
                     Login in With Google
                   </button>
                 </div>
